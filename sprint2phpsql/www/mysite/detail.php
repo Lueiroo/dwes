58e23eddf1cabf90e83b8965ba4c2bdc41d7818e
Diego <dlueiroc@fpcoruna.afundacion.org>
2022-10-17 13:09:50 +0200

    Ejercicio 2.2 apartado 4

--- sprint2phpsql/www/mysite/detail.php
+++ sprint2phpsql/www/mysite/detail.php
@@ -27,5 +27,12 @@
 			mysqli_close($db);
 			echo '</ul>';
 		?>
+
+		<p>Deja un nuevo comentario: </p>
+		<form action="/comment.php" method="post">
+			<textarea rows="4" cols="50" name="new_comment"></textarea><br>
+		<input type="hidden" name="cancion_id" value="<?php echo $cancion_id; ?>">
+			<input type="submit" value="Comentar">
+		</form>
 	</body>
 </html>
