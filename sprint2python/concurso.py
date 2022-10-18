3c3a804e63ed29b9b36a283b02d2b0ef309c5da2
Lueiroo <113440526+Lueiroo@users.noreply.github.com>
2022-10-18 13:59:54 +0200

    concurso.py mejorado un poco

---
+++ sprint2python/concurso.py
@@ -0,0 +1,36 @@
+#FUNCIONES
+def respuesta():
+    r = input("¿Cúal es tu respuesta?")
+    re = r.upper()
+    while (re != 'A' and re != 'B' and re != 'C'):
+        print("Solamente se aceptan como respuesta 'A', 'B' o 'C'. Inténtelo de nuevo.")
+        print ("")
+        r = input("¿Cúal es tu respuesta?")
+        re = r.upper()
+    return r
+
+
+#MAIN
+print ("")
+print ("********** CONCURSO **********")
+print ("")
+
+print ("-------------------------------------------------------------------------------------------")
+print ("¿Cuál es objetivamente (siendo yo el baremo) la mejor canción del nuevo álbum del Ferxxo?")
+print ("-------------------------------------------------------------------------------------------")
+print ("")
+
+print ("Respuestas posibles: ")
+print ("A) Lady mi amor.")
+print ("B) Nieve.")
+print ("C) Si te la encuentras por ahí.")
+print ("")
+
+r = respuesta()
+
+
+print ("")
+if r == 'B':
+    print("Oleeeeee esa era")
+else:
+    print ("Mecachis esa no era")
