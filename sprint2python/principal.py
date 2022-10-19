d3909bef7a19604c7aab416b741e8358bcb65452
Lueiroo <113440526+Lueiroo@users.noreply.github.com>
2022-10-19 14:07:13 +0200

    Modificado principal (ejercicio 6)
    Ahora mide el tiempo ole

--- sprint2python/principal.py
+++ sprint2python/principal.py
@@ -2,6 +2,7 @@
 from ast import If
 from factorial import func_factorial
 from factorial2 import func_factorial_no_recursiva
+import time
 
 def respuesta():
     print ("Desea realizar el factorial de forma recursiva ('A') o de forma no recursiva ('B'): ")
@@ -25,12 +26,21 @@ opcion = respuesta()
 if (opcion == 'A'):
     print ("Elegida la opción A ('De forma recursiva')")
     print ("")
+    tiempo_inicio = time.time()
     print ("El factorial del número introducido calculado de forma recursiva es: " + str(func_factorial(int(n))))
+    tiempo_final = time.time()
     print ("")
+    tiempo_total = tiempo_final - tiempo_inicio
+    print ("El tiempo total en ejecutar la función es de: " + str(tiempo_total))
+
 else:
     print ("Elegida la opción B ('De forma no recursiva')")
     print ("")
+    tiempo_inicio = time.time()
     print ("El factorial del número introducido calculado de forma no recursiva es: " + str(func_factorial_no_recursiva(int(n))))
+    tiempo_final = time.time()
     print ("")
+    tiempo_total = tiempo_final - tiempo_inicio
+    print ("El tiempo total en ejecutar la función es de: " + str(tiempo_total))
 
 
