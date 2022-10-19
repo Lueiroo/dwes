87b57e0ffcc7ace21ca6eb169a7d052fec624c9d
Lueiroo <113440526+Lueiroo@users.noreply.github.com>
2022-10-19 13:55:22 +0200

    Modificado principal (ejercicio 5)

--- sprint2python/principal.py
+++ sprint2python/principal.py
@@ -1,11 +1,36 @@
 
+from ast import If
 from factorial import func_factorial
 from factorial2 import func_factorial_no_recursiva
 
+def respuesta():
+    print ("Desea realizar el factorial de forma recursiva ('A') o de forma no recursiva ('B'): ")
+    opcion = (input("Introduzca su opción: "))
+    opcion = opcion.upper()
+    while(opcion != 'A' and opcion != 'B'):
+        print ("")
+        print ("Respuesta incorrecta, sólo se acepta 'A' o 'B' ")
+        print ("")
+        opcion = (input("Introduzca su opción: "))
+        opcion = opcion.upper()
+    return opcion
+
+#MAIN
+
+print ("")
 n = (input("Introduzca un número: "))
-
-print ("El factorial del número introducido calculado de forma recursiva es: " + str(func_factorial(int(n))))
 print ("")
 
-print ("El factorial del número introducido calculado de forma no recursiva es: " + str(func_factorial_no_recursiva(int(n))))
-print ("")
+opcion = respuesta()
+if (opcion == 'A'):
+    print ("Elegida la opción A ('De forma recursiva')")
+    print ("")
+    print ("El factorial del número introducido calculado de forma recursiva es: " + str(func_factorial(int(n))))
+    print ("")
+else:
+    print ("Elegida la opción B ('De forma no recursiva')")
+    print ("")
+    print ("El factorial del número introducido calculado de forma no recursiva es: " + str(func_factorial_no_recursiva(int(n))))
+    print ("")
+
+
