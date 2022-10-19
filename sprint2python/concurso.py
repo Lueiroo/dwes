60b88c9ea03bfc7ca3e87d6ef94c4d6ecab7415e
Lueiroo <113440526+Lueiroo@users.noreply.github.com>
2022-10-19 14:24:26 +0200

    Modificado concurso (ejercicio 7)
    Solo pregunta dos elegidas aleatoriamente en lugar de 3

--- sprint2python/concurso.py
+++ sprint2python/concurso.py
@@ -1,4 +1,7 @@
 #FUNCIONES
+from random import randint
+
+
 def respuesta():
     r = input("¿Cúal es tu respuesta?")
     re = r.upper()
@@ -11,26 +14,81 @@ def respuesta():
 
 
 #MAIN
+puntuacion = 0
+
+pregunta_no_elegida = randint(1,3)
+
 print ("")
 print ("********** CONCURSO **********")
 print ("")
 
-print ("-------------------------------------------------------------------------------------------")
-print ("¿Cuál es objetivamente (siendo yo el baremo) la mejor canción del nuevo álbum del Ferxxo?")
-print ("-------------------------------------------------------------------------------------------")
+if (pregunta_no_elegida != 1):
+    print ("-------------------------------------------------------------------------------------------")
+    print ("PRIMERA PREGUNTA")
+    print ("¿Cuál es objetivamente (siendo yo el baremo) la mejor canción del nuevo álbum del Ferxxo?")
+    print ("-------------------------------------------------------------------------------------------")
+    print ("")
+
+    print ("Respuestas posibles: ")
+    print ("A) Lady mi amor.")
+    print ("B) Nieve.")
+    print ("C) Si te la encuentras por ahí.")
+    print ("")
+
+    r = respuesta()
+
+    print ("")
+    if r == 'B':
+        print("Oleeeeee esa era +5 puntos para ti!")
+        puntuacion += 5
+    else:
+        print ("Mecachis esa no era")
+
+
+if (pregunta_no_elegida != 2):
+    print ("-------------------------------------------------------------------------------------------")
+    print ("SEGUNDA PREGUNTA")
+    print ("¿Cuál es la mejor parte de 'Cayó la noche REMIX'?")
+    print ("-------------------------------------------------------------------------------------------")
+    print ("")
+
+    print ("Respuestas posibles: ")
+    print ("A) La de Quevedo.")
+    print ("B) La de Bejo.")
+    print ("C) La de Cruz Cafuné.")
+    print ("")
+
+    r = respuesta()
+
+    print ("")
+    if r == 'A':
+        print("Oleeeeee esa era +5 puntos para ti!")
+        puntuacion += 5
+    else:
+        print ("Mecachis esa no era")
+
+
+if (pregunta_no_elegida != 3):
+    print ("-------------------------------------------------------------------------------------------")
+    print ("TERCERA PREGUNTA")
+    print ("¿Cuál es la última canción que escucho Iago Morales el 18/10/2022 a las 14:11?")
+    print ("-------------------------------------------------------------------------------------------")
+    print ("")
+
+    print ("Respuestas posibles: ")
+    print ("A) Cerebros destruidos de Escorbuto.")
+    print ("B) Toxic de Britney Spears.")
+    print ("C) Legalización de SKA-P.")
+    print ("")
+
+    r = respuesta()
+
+    print ("")
+    if r == 'A':
+        print("Oleeeeee esa era +5 puntos para ti!")
+        puntuacion += 5
+    else:
+        print ("Mecachis esa no era")
+
 print ("")
-
-print ("Respuestas posibles: ")
-print ("A) Lady mi amor.")
-print ("B) Nieve.")
-print ("C) Si te la encuentras por ahí.")
-print ("")
-
-r = respuesta()
-
-
-print ("")
-if r == 'B':
-    print("Oleeeeee esa era")
-else:
-    print ("Mecachis esa no era")
+print ("El resultado final es de " + str(puntuacion))
