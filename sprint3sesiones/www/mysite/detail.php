693a9018918c0a2a4c46793f31c0baea91280b7e
Diego <dlueiroc@fpcoruna.afundacion.org>
2022-10-20 10:56:08 +0200

    Ejercicio 1 sprint3sesiones

---
+++ sprint3sesiones/www/mysite/detail.php
@@ -0,0 +1,38 @@
+<html>
+	<body>
+		<?php
+			$db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
+		?>
+		<h1>Conexión establecida</h1>
+		<?php
+			if (!isset($_GET['cancion_id'])) {
+				die('No se ha especificado una canción');
+			}
+			$cancion_id = $_GET['cancion_id'];
+			$query = 'SELECT * FROM tCanciones WHERE id='.$cancion_id;
+			$result = mysqli_query($db, $query) or die('Query error2');
+			$only_row = mysqli_fetch_array($result);
+			echo '<h1>'.$only_row[1].'</h1>';
+			echo '<img src="/Imágenes/'.$only_row[2];
+				echo'">';
+			echo '<h2>'.$only_row[3].'</h2>';
+			echo '<h2>'.$only_row[4].'</h2>';
+			echo '<br><br><h3>Comentarios: </h3>';
+			echo '<ul>';
+			$query_2 = 'SELECT * FROM tComentarios WHERE cancion_id='.$cancion_id;
+			$result_2 = mysqli_query($db, $query_2) or die('Query error2');
+			while ($row = mysqli_fetch_array($result_2)) {
+				echo '<li>'.$row['comentario'].'</li><p>'.$row['fecha'].'</p>';
+			}
+			mysqli_close($db);
+			echo '</ul>';
+		?>
+
+		<p>Deja un nuevo comentario: </p>
+		<form action="/comment.php" method="post">
+			<textarea rows="4" cols="50" name="new_comment"></textarea><br>
+		<input type="hidden" name="cancion_id" value="<?php echo $cancion_id; ?>">
+			<input type="submit" value="Comentar">
+		</form>
+	</body>
+</html>
---
+++ sprint3sesiones/www/mysite/main.php
@@ -0,0 +1,32 @@
+<html>
+	<body>
+		<?php
+			$db = mysqli_connect('localhost', 'root', '1234', 'mysitedb') or die('Fail');
+		?>
+		<h1>Conexión establecida</h1>
+		<?php
+			$query =  "SELECT * FROM tCanciones";
+			$result = mysqli_query($db, $query) or die('Query error');
+			echo"<table>";
+			echo"<tr>";
+				echo"<th>ID</th>";
+				echo"<th>Nombre</th>";
+				echo"<th>URL</th>";
+				echo"<th>Autoría</th>";
+				echo"<th>Fecha de publicación</th>";
+			echo"</tr>";
+			while ($row = mysqli_fetch_array($result)) {
+				echo"<tr>";
+					echo"<td>".$row[0]."</td>";
+					echo"<td>" .$row[1]."</td>";
+					echo'<td><img src="/Imágenes/'.$row[2];
+						echo'"></td>';
+					echo"<td>" .$row[3]."</td>";
+					echo"<td>".$row[4]."</td>";
+				echo"</tr>";
+			}
+			echo"</table>";
+			mysqli_close($db);
+			?>
+	</body>
+</html>
